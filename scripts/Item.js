97dae09f0915bf95fc1081f469297989074d98fd
Rich Greenhill <rgnet1@yahoo.com>
2018-01-03 13:54:59 -0800

    convert to standardized reveal pattern

--- scripts/Item.js
+++ scripts/Item.js
@@ -3,18 +3,21 @@
 // eslint-disable-next-line no-unused-vars
 const Item = (function(){
 
-  return {
-    validateName: function(name, action) {
-      if (!name) throw new TypeError(`Cannot ${action || 'perform action'}: Name must not be blank`);
-    },
-
-    createItem: function(name) {
-      return {
-        id: cuid(),
-        name,
-        checked: false
-      };
-    }
+  const validateName = function(name, action) {
+    if (!name) throw new TypeError(`Cannot ${action || 'perform action'}: Name must not be blank`);
   };
 
+  const createItem = function(name) {
+    return {
+      id: cuid(),
+      name,
+      checked: false
+    };
+  };
+
+  return {
+    validateName,
+    createItem
+  };
+  
 }());
