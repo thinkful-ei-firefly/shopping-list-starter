1b8db9941493f94932d9512aecdf57a6b4b5c05c
Rich Greenhill <rgnet1@yahoo.com>
2018-01-03 13:57:24 -0800

    convert store to standardized reveal pattern

--- scripts/store.js
+++ scripts/store.js
@@ -2,43 +2,50 @@
 
 // eslint-disable-next-line no-unused-vars
 const store = (function(){
+  const addItem = function(name) {
+    Item.validateName(name, 'create item');
+    this.items.push(Item.createItem(name));
+  };
+
+  const findById = function(id) {
+    return this.items.find(item => item.id === id);
+  };
+
+  const findAndToggleChecked = function(id) {
+    const item = this.findById(id);
+    item.checked = !item.checked;
+  };
+
+  const findAndDelete = function(id) {
+    this.items = this.items.filter(item => item.id !== id);
+  };
+
+  const findAndUpdateName = function(id, name) {
+    Item.validateName(name, 'update name');
+    const item = this.findById(id);
+    item.name = name;
+  };
+
+  const toggleCheckedFilter = function() {
+    this.hideCheckedItems = !this.hideCheckedItems;
+  };
+
+  const setSearchTerm = function(term) {
+    this.searchTerm = term;
+  };
 
   return {
     items: [],
     hideCheckedItems: false,
     searchTerm: '',
 
-    addItem(name) {
-      Item.validateName(name, 'create item');
-      this.items.push(Item.createItem(name));
-    },
-
-    findById(id) {
-      return this.items.find(item => item.id === id);
-    },
-
-    findAndToggleChecked(id) {
-      const item = this.findById(id);
-      item.checked = !item.checked;
-    },
-
-    findAndDelete(id) {
-      this.items = this.items.filter(item => item.id !== id);
-    },
-
-    findAndUpdateName(id, name) {
-      Item.validateName(name, 'update name');
-      const item = this.findById(id);
-      item.name = name;
-    },
-
-    toggleCheckedFilter() {
-      this.hideCheckedItems = !this.hideCheckedItems;
-    },
-
-    setSearchTerm(term) {
-      this.searchTerm = term;
-    }
+    addItem,
+    findById,
+    findAndToggleChecked,
+    findAndDelete,
+    findAndUpdateName,
+    toggleCheckedFilter,
+    setSearchTerm,
   };
   
 }());
