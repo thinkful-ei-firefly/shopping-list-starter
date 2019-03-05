720996123aed8d2c8d3d706fe58c460953c1842e
MrSkinny <rgnet1@yahoo.com>
2019-03-04 19:43:55 -0800

    more comments, minor change to error flow

--- scripts/api.js
+++ scripts/api.js
@@ -3,9 +3,12 @@ const api = (function(){
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/rich';
 
   /**
-   * listApiFetch - Wrapper function for native `fetch` to standardize error handling
+   * listApiFetch - Wrapper function for native `fetch` to standardize error handling. 
    * @param {string} url 
    * @param {object} options 
+   * @returns {Promise} - resolve on all 2xx responses with JSON body
+   *                    - reject on non-2xx and non-JSON response with 
+   *                      Object { code: Number, message: String }
    */
   const listApiFetch = function(...args) {
     // setup var in scope outside of promise chain
@@ -13,14 +16,15 @@ const api = (function(){
     return fetch(...args)
       .then(res => {
         if (!res.ok) {
-          // if response is not 2xx, indicate error occurred
+          // if response is not 2xx, start building error object
           error = { code: res.status };
-        }
 
-        // if response is not JSON type, immediately reject with response Status Text
-        if (!res.headers.get('content-type').includes('json')) {
-          error.message = res.statusText;
-          return Promise.reject(error);
+          // if response is not JSON type, place Status Text in error object and
+          // immediately reject promise
+          if (!res.headers.get('content-type').includes('json')) {
+            error.message = res.statusText;
+            return Promise.reject(error);
+          }
         }
 
         // otherwise, return parsed JSON
